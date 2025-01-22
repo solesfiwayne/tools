219a9f6c852954f23f68a37b5f76e13ad9de3ef7
solesfiwayne <solesfiwayne7w@gmail.com>
2025-01-22 22:05:49 +0200

    Update update_autoconfigs_imap.py

--- update_autoconfigs_imap.py
+++ update_autoconfigs_imap.py
@@ -2,7 +2,7 @@
 
 """
 	This script is intended to run from time to time,
-	to update "autoconfigs.txt" file.
+	to update "autoconfigs.txt" file with IMAP configurations.
 	Actually, you can even don't touch it - it's ok.
 """
 
@@ -29,9 +29,10 @@ def fetcher(jobs_queue, results_queue):
 			xml = requests.get(autoconfig_url + domain, timeout=3).text
 		except:
 			xml = ''
-		imap_host = re.findall(r'<incomingServer type="imap">[\\s\\S]+?<hostname>([\\w.-]+)</hostname>', xml)
-		imap_port = re.findall(r'<incomingServer type="imap">[\\s\\S]+?<port>([\\d]+)</port>', xml)
-		imap_login_template = re.findall(r'<incomingServer type="imap">[\\s\\S]+?<username>([\\w.%@]+)</username>', xml)
+		# Извлечение IMAP-конфигурации
+		imap_host = re.findall(r'<incomingServer type="imap">[\s\S]+?<hostname>([\w.-]+)</hostname>', xml)
+		imap_port = re.findall(r'<incomingServer type="imap">[\s\S]+?<port>([\d]+)</port>', xml)
+		imap_login_template = re.findall(r'<incomingServer type="imap">[\s\S]+?<username>([\w.%]+)</username>', xml)
 		if imap_host and imap_port and imap_login_template:
 			results_queue.put((domain, imap_host[0], imap_port[0], imap_login_template[0]))
 	time.sleep(1)
@@ -39,7 +40,7 @@ def fetcher(jobs_queue, results_queue):
 
 signal.signal(signal.SIGINT, quit)
 
-domain_list = re.findall(r'<a href="([\\w.-]+)">', requests.get(autoconfig_url, timeout=3).text)
+domain_list = re.findall(r'<a href="([\w.-]+)">', requests.get(autoconfig_url, timeout=3).text)
 for domain in domain_list:
 	jobs_queue.put(domain)
 
@@ -48,10 +49,10 @@ while threads_counter < 30:
 	threads_counter += 1
 
 with open(filename, 'a') as fp:
-	fp.write('fetched from: ' + autoconfig_url + ', updated at: ' + str(datetime.date.today()) + '\\n')
-	fp.write('domain:imap_host:imap_port:imap_login_template\\n')
+	fp.write('fetched from: ' + autoconfig_url + ', updated at: ' + str(datetime.date.today()) + '\n')
+	fp.write('domain:imap_host:imap_port:imap_login_template\n')
 	while threads_counter > 0:
 		if not results_queue.empty():
 			single_conf_string = ':'.join(results_queue.get())
-			fp.write(single_conf_string + '\\n')
+			fp.write(single_conf_string + '\n')
 			print(single_conf_string)
