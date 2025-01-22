b5775bf70f35b7e2f69cc63d7a1e9ed94e9aad04
solesfiwayne <solesfiwayne7w@gmail.com>
2025-01-22 21:26:29 +0200

    Update update_autoconfigs_imap.py

--- update_autoconfigs_imap.py
+++ update_autoconfigs_imap.py
@@ -1,17 +1,22 @@
 #!/usr/local/bin/python3
 
 """
-    This script is intended to run from time to time,
-    to update "autoconfigs.txt" file.
-    Actually, you can even don't touch it - it's ok.
+    This script updates "autoconfigs.txt" file with IMAP configurations.
 """
 
-import os, sys, threading, time, re, signal, queue, requests, datetime
+import os
+import sys
+import threading
+import time
+import re
+import signal
+import queue
+import requests
+import datetime
 
 filename = 'autoconfigs.txt'
 autoconfig_url = 'https://autoconfig.thunderbird.net/v1.1/'
 threads_counter = 0
-today = datetime.date.today()
 jobs_queue = queue.Queue()
 results_queue = queue.Queue()
 
@@ -21,38 +26,48 @@ def quit(signum, frame):
 
 def fetcher(jobs_queue, results_queue):
     global threads_counter
-    while True:
-        if jobs_queue.empty():
-            break
+    while not jobs_queue.empty():
         domain = jobs_queue.get()
         try:
-            xml = requests.get(autoconfig_url + domain, timeout=3).text
-        except:
+            xml = requests.get(autoconfig_url + domain, timeout=5).text
+        except requests.RequestException:
             xml = ''
-        imap_host = re.findall(r'<incomingServer type="imap">[\s\S]+?<hostname>([\w.-]+)</hostname>', xml)
-        imap_port = re.findall(r'<incomingServer type="imap">[\s\S]+?<port>([\d]+)</port>', xml)
-        imap_login_template = re.findall(r'<incomingServer type="imap">[\s\S]+?<username>([\w.%]+)</username>', xml)
+        imap_host = re.findall(r'<incomingServer type="imap">[\\s\\S]+?<hostname>([\\w.-]+)</hostname>', xml)
+        imap_port = re.findall(r'<incomingServer type="imap">[\\s\\S]+?<port>([\\d]+)</port>', xml)
+        imap_login_template = re.findall(r'<incomingServer type="imap">[\\s\\S]+?<username>([\\w.%]+)</username>', xml)
         if imap_host and imap_port and imap_login_template:
             results_queue.put((domain, imap_host[0], imap_port[0], imap_login_template[0]))
-    time.sleep(1)
+        jobs_queue.task_done()  # Помечаем задачу как выполненную
     threads_counter -= 1
 
 signal.signal(signal.SIGINT, quit)
 
-domain_list = re.findall(r'<a href="([\w.-]+)">', requests.get(autoconfig_url, timeout=3).text)
-for domain in domain_list:
-    jobs_queue.put(domain)
+# Загружаем список доменов
+try:
+    domain_list = re.findall(r'<a href="([\\w.-]+)">', requests.get(autoconfig_url, timeout=10).text)
+    for domain in domain_list:
+        jobs_queue.put(domain)
+except requests.RequestException as e:
+    print(f"Failed to fetch domain list: {e}")
+    sys.exit(1)
 
-while threads_counter < 30:
+# Запускаем потоки для обработки
+for _ in range(min(30, jobs_queue.qsize())):
     threading.Thread(target=fetcher, args=(jobs_queue, results_queue), daemon=True).start()
     threads_counter += 1
 
-with open(filename, 'a') as fp:
-    fp.write('fetched from: ' + autoconfig_url + ', updated at: ' + str(datetime.date.today()) + '\n')
-    fp.write('domain;imap_host:imap_port;imap_login_template\n')  # Изменён заголовок
-    while threads_counter > 0:
-        if not results_queue.empty():
-            # Изменение формата строки
-            single_conf_string = f"{results_queue.get()[0]};{results_queue.get()[1]}:{results_queue.get()[2]};{results_queue.get()[3]}"
-            fp.write(single_conf_string + '\n')
-            print(single_conf_string)
+# Ожидание завершения потоков
+while threads_counter > 0:
+    time.sleep(0.1)
+
+# Запись результатов в файл
+with open(filename, 'w', encoding='utf-8') as fp:
+    fp.write('fetched from: ' + autoconfig_url + ', updated at: ' + str(datetime.date.today()) + '\\n')
+    fp.write('domain;imap_host:imap_port;imap_login_template\\n')
+    while not results_queue.empty():
+        domain, imap_host, imap_port, imap_login_template = results_queue.get()
+        single_conf_string = f"{domain};{imap_host}:{imap_port};{imap_login_template}"
+        fp.write(single_conf_string + '\\n')
+        print(single_conf_string)
+
+print("Processing complete.")
