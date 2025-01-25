3796c3dbec314ad7920c798b1cc67428a16220a3
solesfiwayne <solesfiwayne7w@gmail.com>
2025-01-25 11:55:19 +0200

    Update update_autoconfigs.py

--- update_autoconfigs.py
+++ update_autoconfigs.py
@@ -1,9 +1,9 @@
 #!/usr/local/bin/python3
 
 """
-	This script is intented to run from time to time,
-	to update "autoconfigs.txt" file.
-	Actually, you can even don't touch it - it's ok.
+    This script is intented to run from time to time,
+    to update "autoconfigs.txt" file.
+    Actually, you can even don't touch it - it's ok.
 """
 
 import os, sys, threading, time, re, signal, queue, requests, datetime
@@ -16,42 +16,41 @@ jobs_queue = queue.Queue()
 results_queue = queue.Queue()
 
 def quit(signum, frame):
-	print('Exiting... See ya later. Bye.')
-	sys.exit(0)
+    print('Exiting... See ya later. Bye.')
+    sys.exit(0)
 
 def fetcher(jobs_queue, results_queue):
-	global threads_counter
-	while True:
-		if jobs_queue.empty():
-			break
-		domain = jobs_queue.get()
-		try:
-			xml = requests.get(autoconfig_url+domain, timeout=3).text
-		except:
-			xml = ''
-		smtp_host = re.findall(r'<outgoingServer type="smtp">[\s\S]+?<hostname>([\w.-]+)</hostname>', xml)
-		smtp_port = re.findall(r'<outgoingServer type="smtp">[\s\S]+?<port>([\d+]+)</port>', xml)
-		smtp_login_template = re.findall(r'<outgoingServer type="smtp">[\s\S]+?<username>([\w.%]+)</username>', xml)
-		if smtp_host and smtp_port and smtp_login_template:
-			results_queue.put((domain, smtp_host[0], smtp_port[0], smtp_login_template[0]))
-	time.sleep(1)
-	threads_counter -= 1
+    global threads_counter
+    while True:
+        if jobs_queue.empty():
+            break
+        domain = jobs_queue.get()
+        try:
+            xml = requests.get(autoconfig_url + domain, timeout=3).text
+        except:
+            xml = ''
+        smtp_configs = re.findall(r'<outgoingServer type="smtp">[\s\S]*?<hostname>([\w.-]+)</hostname>[\s\S]*?<port>([\d]+)</port>[\s\S]*?<username>([\w.%]+)</username>', xml)
+        for smtp_host, smtp_port, smtp_login_template in smtp_configs:
+            results_queue.put((domain, smtp_host, smtp_port, smtp_login_template))
+    time.sleep(1)
+    threads_counter -= 1
 
 signal.signal(signal.SIGINT, quit)
 
 domain_list = re.findall(r'<a href="([\w.-]+)">', requests.get(autoconfig_url, timeout=3).text)
 for domain in domain_list:
-	jobs_queue.put(domain)
+    jobs_queue.put(domain)
 
-while threads_counter<30:
-	threading.Thread(target=fetcher, args=(jobs_queue, results_queue), daemon=True).start()
-	threads_counter += 1
+while threads_counter < 30:
+    threading.Thread(target=fetcher, args=(jobs_queue, results_queue), daemon=True).start()
+    threads_counter += 1
 
 with open(filename, 'a') as fp:
-	fp.write('fetched from: '+autoconfig_url+', updated at: '+str(datetime.date.today())+'\n')
-	fp.write('domain:smtp_host:smtp_port:smtp_login_template\n')
-	while threads_counter>0:
-		if not results_queue.empty():
-			single_conf_string = ':'.join(results_queue.get())
-			fp.write(single_conf_string+'\n')
-			print(single_conf_string)
+    fp.write(f'Fetched from: {autoconfig_url}, updated at: {today}\n')
+    fp.write('Domain;SMTP Host:SMTP Port:SMTP Login Template\n')
+    while threads_counter > 0 or not results_queue.empty():
+        if not results_queue.empty():
+            domain, smtp_host, smtp_port, smtp_login_template = results_queue.get()
+            single_conf_string = f'{domain};{smtp_host}:{smtp_port}:{smtp_login_template}'
+            fp.write(single_conf_string + '\n')
+            print(single_conf_string)
