71fe350a6c924eab9f696f837c5d4bb294f9f9df
solesfiwayne <solesfiwayne7w@gmail.com>
2026-01-03 18:13:20 +0200

    Update mailpass2smtp3.py

--- mailpass2smtp3.py
+++ mailpass2smtp3.py
@@ -80,7 +80,7 @@ def show_banner():
          |█|    `   ██/  ███▌╟█, (█████▌   ╙██▄▄███   @██▀`█  ██ ▄▌             
          ╟█          `    ▀▀  ╙█▀ `╙`╟█      `▀▀^`    ▀█╙  ╙   ▀█▀`             
          ╙█                           ╙                                         
-          ╙     {b}MadCat SMTP Checker & Cracker v44.12.15{z}
+          ╙     {b}MadCat SMTP Checker & Cracker v41.12.15{z}
                 Made by {b}Aels{z} for community: {b}https://xss.is{z} - forum of security professionals
                 https://github.com/aels/mailtools
                 https://t.me/IamLavander
